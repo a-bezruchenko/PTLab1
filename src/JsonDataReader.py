1dbfb4014c27d710cba8b96694ad41bb6a4616ad
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2021-09-28 17:33:50 +0300

    fixed JsonReader format

--- src/JsonDataReader.py
+++ src/JsonDataReader.py
@@ -5,8 +5,10 @@ import json
 class JsonDataReader(DataReader):
     def read(self, path: str) -> DataType:
         file_text : str
-        parsed_file : DataType
+        parsed : DataType
+        result : DataType
         with open(path, "r", encoding="utf-8") as file:
             file_text = file.read()    
-        parsed_file = json.loads(file_text)
-        return parsed_file
+        parsed = json.loads(file_text)
+        result = {x : list(parsed[x].items()) for x in parsed}
+        return result
