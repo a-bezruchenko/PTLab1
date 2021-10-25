4759cf27399cc51aa5b4ee95c691fdc417122209
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2021-10-25 22:43:55 +0300

    pep fixes

--- src/JsonDataReader.py
+++ src/JsonDataReader.py
@@ -8,8 +8,8 @@ class JsonDataReader(DataReader):
         file_text: str
         parsed: DataType
         result: DataType
-        with open(path, "r", encoding = "utf-8") as file:
+        with open(path, "r", encoding="utf-8") as file:
             file_text = file.read()
         parsed = json.loads(file_text)
-        result = {x : list(parsed[x].items()) for x in parsed}
+        result = {x: list(parsed[x].items()) for x in parsed}
         return result
