3314064304ccd4ed27c4c7e7a885cabf9ccd5b78
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2021-10-26 17:19:03 +0300

    pep fixes

--- test/test_GetLastRatingQuartile.py
+++ test/test_GetLastRatingQuartile.py
@@ -11,18 +11,17 @@ class TestCalcRating():
     @pytest.fixture()
     def input_data(self) -> List[Tuple[RatingsType, float]]:
         test_cases: List[Tuple[RatingsType, float]] = [
-            ({"a":1, "b":2, "c":3, "d":4}, 3),
-            ({"a":1, "b":2}, 1.75)
+            ({"a": 1, "b": 2, "c": 3, "d": 4}, 3),
+            ({"a": 1, "b": 2}, 1.75)
             ]
         return test_cases
-        
+
     def test_calc(self, test_cases:
                   List[Tuple[RatingsType, float]]) -> None:
         for test_case in test_cases:
-            rating : RatingsType
-            expected_quartile : float
+            rating: RatingsType
+            expected_quartile: float
             rating, expected_quartile = test_case
-            quartile : float = GetLastRatingQuartile().calc(rating)
+            quartile: float = GetLastRatingQuartile().calc(rating)
             assert pytest.approx(quartile, abs=0.001) == expected_quartile
 
-            
