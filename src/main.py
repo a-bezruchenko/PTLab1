ca2d198394fae630a01439b767d571b0fcc8b05d
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2021-11-23 16:43:38 +0300

    pep fixes

--- src/main.py
+++ src/main.py
@@ -6,12 +6,14 @@ import argparse
 import sys
 from pprint import pprint
 
+
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
+
 
 def main():
     text_path = get_path_from_arguments(sys.argv[1:3])
